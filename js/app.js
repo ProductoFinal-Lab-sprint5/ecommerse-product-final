9bab6c466123e4f22f9ee3986b6e09d884d942de
NatalyOC <noteroc@hotmail.com>
2018-02-25 12:07:59 -0500

    primera version heandlebars

--- js/app.js
+++ js/app.js
@@ -1,26 +1,4 @@
-/*
-//Informacion del usuario
-$.get('https://api.mercadolibre.com/users/303765493?access_token=APP_USR-484452126434281-022416-234e7181c72c5181d4083a302cbdde5b__F_M__-303765493', function(data){
-  console.log(data);
-});
-//Información del sitio
-$.get('https://api.mercadolibre.com/sites/MPE',function(data){
-  console.log(data);
-})
-//Devuelve las categorías disponibles en el sitio.
-$.get('https://api.mercadolibre.com/sites/MPE/categories',function(data){
-  console.log(data);
-})*/
-// Devuelve información sobre la categoría.
-$.get('https://api.mercadolibre.com/categories/MPE1039', function(data) {
-  console.log(data);
-  // console.log(data.children_categories);
-});
 
-$.get('https://api.mercadolibre.com/sites/MPE/search?category=MPE1039', function(data) {
-  console.log(data);
-  // console.log(data.children_categories);
-});
 let token = 'APP_USR-484452126434281-022418-b31c5d304dcc66db9034b963dad75b59__C_J__-303765493';
 let indexCat = '';
 /* SubCate */
